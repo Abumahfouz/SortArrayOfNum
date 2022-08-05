60e9b8f8e61d147d5c6a54232a7732481d8a7fc8
Support <Support@WEMA-TRAINING-S>
2022-08-05 08:46:45 +0100

    Add project files.

---
+++ SortArrayOfNum/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SortArrayOfNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] newNumbers = new int[] {21,7,5,12,11,16,4,10,20,9,7,15};
+            int dummy;
+
+            for (int i = 0; i < newNumbers.Length; i++)
+            {
+                for (int sort = i+1; sort < newNumbers.Length; sort++)
+                {
+                    if (newNumbers[i] < newNumbers[sort])
+                    {
+                        dummy = newNumbers[i];
+                        newNumbers[i] = newNumbers[sort];
+                        newNumbers[sort] = dummy;
+                    }
+                }
+            }
+            foreach (int values in newNumbers)
+            {
+                Console.WriteLine(values + " ");
+            }
+
+        }
+    }
+}
